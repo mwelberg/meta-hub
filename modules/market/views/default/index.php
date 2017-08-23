43ba5c190c2d96efcc70a1c77da417e82b51d1d8
mwelberg <moritz@welberg.de>
2017-08-23 22:27:23 +0200

    added GET parameter item_id to buy button on market/index

--- modules/market/views/default/index.php
+++ modules/market/views/default/index.php
@@ -26,29 +26,29 @@
        </tr>
      </thead>
    <?php
-     $form = ActiveForm::begin([
+     /*$form = ActiveForm::begin([
          'id' => 'market-form',
          'layout' => 'horizontal',
          'fieldConfig' => [
              'template' => "<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
          ],
          'options' => ['enctype' => 'multipart/form-data'],
-     ]);
+     ]);*/
      foreach ($items as $item): ?>
      <tr>
        <?php
          echo '<td>'.$item->ID.'</td>';
          echo '<td>'.$item->name.'</td>';
          echo '<td>'.$item->price.'</td>';
-         echo Html::HiddenInput('item_id', $item->ID);
-         echo Html::HiddenInput('user_id', Yii::$app->user->identity->ID);
+         //echo Html::HiddenInput('item_id', $item->ID);
+         //echo Html::HiddenInput('user_id', Yii::$app->user->identity->ID);
          //echo '<td>'.$form->field($model, 'quantity')->textInput(['type' => 'number']).'</td>';
-         echo '<td>'.Html::submitButton('Buy', ['class' => 'btn btn-primary', 'name' => 'buy-button']).'</td>';
+         echo '<td>'.Html::a('Buy', ['/market','item_id' => $item->ID],['class' => 'btn btn-primary', 'name' => 'buy-button']).'</td>';
        ?>
      </tr>
     <?php
       endforeach;
-      ActiveForm::end();
+      //ActiveForm::end();
     ?>
   </table>
   <?= LinkPager::widget(['pagination' => $pagination]) ?>
